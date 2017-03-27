72955912378fc03501aed44bff3198a421c4ad16
Prashant Dey <prashantsavior@gmail.com>
2017-03-27 00:48:55 -0700

    Deleted unnecessary files

--- Wireless Project/dump1090.sh
+++
@@ -1,6 +0,0 @@
-# This is bash script for the dump1090
-git clone https://www.github.com/UnpredictablePrashant/dump1090.git
-sudo apt-get install librtlsdr
-cd dump1090
-make
-./dump1090 --enable-agc --aggressive --net-http-port 8080
